5ecadd4ef566682c500a87f233bdbc66fad91c51
Jawahirul Fanani <kyuraz029@gmail.com>
2024-01-19 09:31:44 +0700

    Insert data geojson to postgresql

---
+++ geo.py
@@ -0,0 +1,52 @@
+import psycopg2
+import json
+
+with open ('data/Des_Kel_YD.geojson') as file:
+    geojson_data = json.load(file)
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="geojson",
+    user="postgres",
+    password="kyuura20020929",
+    port="5432"
+)
+
+cur = conn.cursor()
+
+cur.execute("""
+    CREATE TABLE desa_kel_yd (
+        objectid VARCHAR PRIMARY KEY,
+        namobj VARCHAR,
+        kdcpum VARCHAR,
+        wadmkc VARCHAR,
+        wadmkd VARCHAR,
+        wadmkk VARCHAR,
+        wadmpr VARCHAR,
+        luas NUMERIC,
+        coordinates JSONB
+    )
+""")
+conn.commit()
+
+for feature in geojson_data['features']:
+    objectid = feature['properties']['OBJECTID']
+    namobj = feature['properties']['NAMOBJ']
+    kdcpum = feature['properties']['KDCPUM']
+    wadmkc = feature['properties']['WADMKC']
+    wadmkd = feature['properties']['WADMKD']
+    wadmkk = feature['properties']['WADMKK']
+    wadmpr = feature['properties']['WADMPR']
+    luas = feature['properties']['luas']
+    coordinates = json.dumps(feature['geometry']['coordinates'])
+    
+    cur.execute("""
+        INSERT INTO desa_kel_yd (objectid, namobj, kdcpum, wadmkc, wadmkd, wadmkk, wadmpr, luas, coordinates)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """, (objectid, namobj, kdcpum, wadmkc, wadmkd, wadmkk, wadmpr, luas, coordinates))
+    
+conn.commit()
+
+cur.close()
+
+conn.close()
